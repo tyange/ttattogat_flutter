81d39ebdd99d796e716ecdb4b005c0520c80d3d6
babyazalea <usun12@hotmail.com>
2021-10-29 00:56:28 +0900

    testing core structure

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -6,11 +6,25 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const CupertinoPageScaffold(
-      navigationBar: CupertinoNavigationBar(
+    return CupertinoPageScaffold(
+      navigationBar: const CupertinoNavigationBar(
         middle: Text('Home'),
       ),
-      child: Text('is home'),
+      child: ListView(
+        children: <Widget>[
+          const FittedBox(
+            child: Text('기념일'),
+          ),
+          FittedBox(
+            child: Row(
+              children: const <Widget>[
+                Text('가계부'),
+                Text('데이트 회의 개최'),
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
