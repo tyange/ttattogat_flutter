c76fe27c65644cdf6c3fd4e6d0ce5804da251b06
babyazalea <usun16@gmail.com>
2022-01-10 16:46:20 +0900

    modified color & text scheme

--- lib/main.dart
+++ lib/main.dart
@@ -16,30 +16,38 @@ class MyApp extends StatelessWidget {
       title: '따로또같이',
       home: EventsScreen(),
       theme: ThemeData(
-        // Define the default brightness and colors.
-
-        colorScheme: const ColorScheme(
-          primary: Color.fromRGBO(255, 196, 61, 1),
-          error: Color.fromRGBO(224, 122, 95, 1),
-          background: Color.fromRGBO(250, 249, 249, 1),
-          onPrimary: Color.fromRGBO(255, 183, 3, 1),
-          onError: Color.fromRGBO(214, 40, 40, 1),
-          onSecondary: Color.fromRGBO(251, 133, 0, 1),
-          secondaryVariant: Color.fromRGBO(224, 120, 0, 1),
-          primaryVariant: Color.fromRGBO(224, 161, 0, 1),
-          onSurface: Color.fromRGBO(244, 241, 222, 1),
-          surface: Color.fromRGBO(244, 241, 222, 1),
-          secondary: Color.fromRGBO(255, 150, 31, 1),
-          brightness: Brightness.light,
-          onBackground: Color.fromRGBO(241, 250, 238, 1),
-        ),
-
-        // Define the default font family.
-        fontFamily: 'Pretendard',
-
-        // Define the default `TextTheme`. Use this to specify the default
-        // text styling for headlines, titles, bodies of text, and more.
-      ),
+          // Define the default brightness and colors.
+          colorScheme: const ColorScheme.light(
+            primary: Colors.amber,
+          )
+              .copyWith(secondary: Colors.indigo[100])
+              .copyWith(error: Colors.redAccent)
+              .copyWith(
+                background: Colors.white,
+              )
+              .copyWith(
+                surface: Colors.grey[100],
+              ),
+          // Define the default font family.
+          fontFamily: 'Pretendard',
+          // Define the default `TextTheme`. Use this to specify the default
+          // text styling for headlines, titles, bodies of text, and more.
+          textTheme: const TextTheme(
+              headline6: TextStyle(
+                fontFamily: 'Gowun Dodum',
+                color: Colors.black,
+                fontSize: 20,
+              ),
+              bodyText1: TextStyle(
+                fontFamily: 'Pretendard',
+                color: Colors.black87,
+                fontSize: 16,
+              ),
+              bodyText2: TextStyle(
+                fontFamily: 'Pretendard',
+                color: Colors.black87,
+                fontSize: 14,
+              ))),
     );
   }
 }
