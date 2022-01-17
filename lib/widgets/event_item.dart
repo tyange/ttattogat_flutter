c345bcdd9393a78ea420a612deb65a1eb2eaf464
babyazalea <usun16@gmail.com>
2022-01-17 21:15:49 +0900

    added dismissible

--- lib/widgets/event_item.dart
+++ lib/widgets/event_item.dart
@@ -8,30 +8,57 @@ class EventItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      constraints: const BoxConstraints(
-        minHeight: 50,
-      ),
-      decoration: BoxDecoration(
-        border: Border.all(
-          color: Colors.black26,
-          width: 1,
-          style: BorderStyle.solid,
-        ),
-        borderRadius: const BorderRadius.all(
-          Radius.circular(10),
+    return Dismissible(
+      key: ValueKey(event.id),
+      background: Container(
+        color: Theme.of(context).errorColor,
+        alignment: Alignment.centerRight,
+        child: const Icon(
+          Icons.delete,
+          color: Colors.white,
         ),
       ),
-      margin: const EdgeInsets.fromLTRB(0, 0, 0, 10),
-      padding: const EdgeInsets.fromLTRB(13, 5, 13, 5),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: <Widget>[
-          Text(event.title),
-          Text(
-            "₩ ${event.amount.toString()}",
+      direction: DismissDirection.endToStart,
+      confirmDismiss: (direction) {
+        return showDialog(
+          context: context,
+          builder: (ctx) => AlertDialog(
+            title: Icon(Icons.delete),
+            content: Text('이벤트를 삭제하시겠어요?'),
+            actions: <Widget>[
+              TextButton(
+                child: Text('아뇨'),
+                onPressed: () {},
+              ),
+              TextButton(
+                child: Text("네"),
+                onPressed: () {},
+              )
+            ],
           ),
-        ],
+        );
+      },
+      child: Card(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Text(
+                event.title,
+                style: const TextStyle(
+                  fontSize: 13,
+                ),
+              ),
+              Text(
+                "₩ ${event.amount.toString()}",
+                style: const TextStyle(
+                  fontSize: 13,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
